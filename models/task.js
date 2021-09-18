db968c16576aa3dafaac5bee243e1bc23e2edfa6
Matt Kearns <clocktower39@aol.com>
2021-09-17 22:30:11 -0700

    bug fix: remove index from task title

--- models/task.js
+++ models/task.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    title: { type: String, required: true, index: { unique: true } },
+    title: { type: String, required: true },
     goal: { type: Number, required: true },
     achieved: { type: Number, required: true },
     accountId: { type: String, required: true },
