397808d80bfe58fda01208ee106302a5a7593ee9
Matt Kearns <clocktower39@aol.com>
2021-09-19 22:53:45 -0700

    update default tasks controller and route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/login', userController.login_user);
 router.post('/signup', userController.signup_user);
+router.post('/updateDefaultTasks', userController.update_default_tasks);
 
 module.exports = router;
