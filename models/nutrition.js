28117cf4bae54079e69402b4fc9464894a099d68
Matt Kearns <clocktower39@aol.com>
2021-09-22 23:07:33 -0700

    adjust nurition model, empty request creates new Nutrition entry in DB

--- models/nutrition.js
+++ models/nutrition.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const nutritionSchema = new mongoose.Schema({
-    title: { type: String, required: true, index: { unique: true } },
+    title: { type: String, required: true },
     unit: { type: String, required: true },
     goal: { type: Number, required: true },
     achieved: { type: Number, required: true },
