9c908395716d66574401cd3f1481bf57aaef6900
clocktower39 <clocktower39@aol.com>
2021-11-26 23:47:54 -0700

    get_weekly_nutrition route/controller

--- controllers/nutritionController.js
+++ controllers/nutritionController.js
@@ -29,14 +29,37 @@ const update_nutrition = (req, res) => {
 }
 
 const get_nutrition = (req, res) => {
-    Nutrition.find({ accountId: req.body.accountId, date: req.body.date }, function(err, data) {
+    Nutrition.find({ accountId: req.body.accountId, date: new Date(req.body.date) }, function(err, data) {
         if(err) throw err;
         res.send(data);
     });
+}
+
+const get_weekly_nutrition = (req, res) => {
+
+    let loopDate =  new Date(req.body.startDate);
+    let endDate = new Date(req.body.endDate);
+    let week = [];
+    
+    while(loopDate <= endDate){
+        week.push(loopDate)
+        loopDate = new Date(new Date(loopDate).getTime() + 1 * (24 * 60 * 60 * 1000));
+    }
+
+    Nutrition.find({
+        $or: week.map(day=> {
+            return {'date': day};
+        })
+      }, function(err, data) {
+        if(err) throw err;
+        res.send(data);
+    });    
+
 }
 
 module.exports = {
     create_nutrition,
     get_nutrition,
     update_nutrition,
+    get_weekly_nutrition,
 }
--- routes/nutritionRoutes.js
+++ routes/nutritionRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/nutrition', auth, nutritionController.get_nutrition);
 router.post('/createNutrition', auth, nutritionController.create_nutrition);
 router.post('/updateNutrition', auth, nutritionController.update_nutrition);
+router.post('/nutritionWeek', auth, nutritionController.get_weekly_nutrition);
 
 module.exports = router;
