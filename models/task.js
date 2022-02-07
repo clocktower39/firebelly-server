67d9c6b373c93d3a011e69c498ddcfcf70ec29e1
clocktower39 <clocktower39@aol.com>
2022-02-06 23:35:23 -0700

    task model redesign

--- models/task.js
+++ models/task.js
@@ -2,41 +2,28 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
     accountId: { type: String, required: true },
-    date: { type: Date, required: true },
-    tasks: {
+    history: {
         type: [
             {
-                title: { type: String },
-                goal: { type: Number },
-                achieved: { type: Number },
+                date: { type: String, required: true, index: { unique: true } },
+                tasks: {
+                    type: [
+                        {
+                            title: { type: String },
+                            goal: { type: Number },
+                            achieved: { type: Number },
+                        }
+                    ]
+                }
             },
         ],
-        default: [
-            
-        ],
-        required: true,
-    }
-    // history: {
-    //     type: [
-    //         {
-    //             date: { type: String, required: true, index: { unique: true } },
-    //             tasks: {
-    //                 type: [
-    //                     {
-    //                         title: { type: String },
-    //                         goal: { type: Number },
-    //                         achieved: { type: Number },
-    //                     }
-    //                 ]
-    //             }
-    //         },
-    //     ],
-    //     default: [],
-    //     required: true
-    // },
-    // defaultTasks: {
-    //     type: []
-    // },
+        default: [],
+        required: true
+    },
+    defaultTasks: {
+        type: [],
+        required: true
+    },
 })
 
 const Task = mongoose.model('Task', taskSchema);
