baf9bb2d94cc65d47a38f1335c2aebf595f3995f
clocktower39 <clocktower39@aol.com>
2022-02-22 16:49:26 -0700

    save changes to default tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/tasks', auth, taskController.get_tasks);
 router.post('/updateTaskHistoryDate', auth, taskController.update_task_history_date);
+router.post('/updateDefaultTasks', auth, taskController.update_default_tasks);
 
 module.exports = router;
