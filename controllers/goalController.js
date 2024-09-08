c256f1f06f878783088449fe128bbc1933027d9d
clocktower39 <clocktower39@aol.com>
2024-09-07 19:42:17 -0700

    required mongoose fixes for upgrade v6 to v8

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,97 +1,101 @@
-const Goal = require('../models/goal');
-const Relationship = require('../models/relationship');
+const Goal = require("../models/goal");
+const Relationship = require("../models/relationship");
 
 const create_goal = (req, res, next) => {
-    let goal = new Goal({
-        ...req.body,
-        createdDate: new Date(),
-        user: res.locals.user._id,
-    });
-    let saveGoal = () => {
-        goal.save((err) => {
-            if (err) return next(err);
-            res.send(goal)
-        });
-    }
-    saveGoal();
-}
+  let goal = new Goal({
+    ...req.body,
+    createdDate: new Date(),
+    user: res.locals.user._id,
+  });
+
+  goal
+    .save()
+    .then((savedGoal) => {
+      res.send(savedGoal);
+    })
+    .catch((err) => next(err));
+};
 
 const remove_goal = (req, res, next) => {
-    Goal.findOneAndDelete({ user: res.locals.user._id, _id: req.body.goalId }, function (err, data) {
-        if (err) {
-            res.send({ error: err })
-        }
-        else {
-            res.send({ status: 'Record deleted' })
-        }
+  Goal.findOneAndDelete({ user: res.locals.user._id, _id: req.body.goalId })
+    .then((data) => {
+      if (!data) {
+        return res.status(404).send({ error: "Goal not found" });
+      }
+      res.send({ status: "Record deleted" });
     })
-}
+    .catch((err) => next(err));
+};
 
 const update_goal = (req, res, next) => {
-    const { title, description, achievedDate, targetDate } = req.body;
-    Goal.findByIdAndUpdate(req.body._id, { title, description, achievedDate, targetDate }, { new: true }, (err, goal) => {
-        if (err) return next(err);
-        else {
-            res.send(goal);
-        }
+  const { title, description, achievedDate, targetDate } = req.body;
+
+  Goal.findByIdAndUpdate(
+    req.body._id,
+    { title, description, achievedDate, targetDate },
+    { new: true }
+  )
+    .then((goal) => {
+      if (!goal) {
+        return res.status(404).send({ error: "Goal not found" });
+      }
+      res.send(goal);
     })
-}
+    .catch((err) => next(err));
+};
 
 const comment_on_goal = (req, res, next) => {
-    const { comment } = req.body;
-    Goal.findById(req.body._id, (err, goal) => {
-        if (err) return next(err);
-        else {
-            const newComment = {
-                createdDate: new Date(),
-                comment,
-            }
-            goal.comments ? goal.comments.push(newComment) : goal.comments = [newComment];
+  const { comment } = req.body;
+  Goal.findById(req.body._id)
+    .then((goal) => {
+      if (!goal) {
+        return res.status(404).send({ error: "Goal not found" });
+      }
+      const newComment = {
+        createdDate: new Date(),
+        comment,
+      };
+      goal.comments ? goal.comments.push(newComment) : (goal.comments = [newComment]);
 
-            let saveGoal = () => {
-                goal.save((err) => {
-                    if (err) return next(err);
-                    res.send(goal)
-                });
-            }
-            saveGoal();
-        }
+      return goal.save();
     })
-}
+    .then((savedGoal) => {
+      res.send(savedGoal);
+    })
+    .catch((err) => next(err));
+};
 
 const get_goals = (req, res, next) => {
-    Goal.find({ user: res.locals.user._id }, function (err, data) {
-        if (err) return next(err);
-        res.send(data || { results: "No Results" });
-    });
-}
+  Goal.find({ user: res.locals.user._id })
+  .then(data => {
+    res.send(data || { results: "No Results" });
+  })
+  .catch(err => next(err));
+};
 
 const get_client_goals = (req, res, next) => {
-    const { client } = req.body;
-    Relationship.findOne({ trainer: res.locals.user._id, client }, (err, relationship) => {
+  const { client } = req.body;
+  Relationship.findOne({ trainer: res.locals.user._id, client })
+  .then(relationship => {
+    if (!relationship) {
+      res.send({ error: "Relationship does not exist." });
+    } else if (relationship.accepted) {
+      Goal.find({ user: client }, function (err, data) {
         if (err) return next(err);
-        
-        if(!relationship){
-            res.send({ error: 'Relationship does not exist.'});
-        }
-        else if(relationship.accepted){
-            Goal.find({ user: client }, function (err, data) {
-                if (err) return next(err);
-                res.send(data);
-            });
-        }
-        else{
-            res.send({ error: 'Relationship pending.'});
-        }
-    })
-
-}
+        res.send(data);
+      });
+    } else {
+      res.send({ error: "Relationship pending." });
+    }
+  })
+  .catch(err => next(err));
+};
 
 module.exports = {
-    create_goal,
-    remove_goal,
-    update_goal,
-    get_goals,
-    comment_on_goal,
-    get_client_goals,
-}
+  create_goal,
+  remove_goal,
+  update_goal,
+  get_goals,
+  comment_on_goal,
+  get_client_goals,
+};
