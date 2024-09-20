6d7d41b8dc7f0fee62aa35ddbc6bbfefe77bfd44
clocktower39 <clocktower39@aol.com>
2024-09-20 00:04:29 -0700

    replace multer-gridfs-storage with mongoose.mongo.GridFSBucket and multer.memoryStorage

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const crypto = require('crypto');
+const path = require('path');
 const { verifyRefreshToken } = require("../middleware/auth");
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
@@ -193,10 +195,9 @@ const get_trainers = (req, res, next) => {
 
 const upload_profile_picture = async (req, res) => {
   try {
-    const connection = await mongoose.connection;
-    const db = connection.getClient().db();
-    let gridfsBucket = new mongoose.mongo.GridFSBucket(db, {
-      bucketName: "profilePicture",
+    const db = mongoose.connection.db;
+    const gridfsBucket = new mongoose.mongo.GridFSBucket(db, {
+      bucketName: "profilePicture"
     });
 
     const user = await User.findById(res.locals.user._id);
@@ -204,28 +205,57 @@ const upload_profile_picture = async (req, res) => {
       return res.status(404).send({ error: "User not found" });
     }
 
+    // Check if the user has a profile picture before deleting
     if (user.profilePicture) {
-      await gridfsBucket.delete(new mongoose.Types.ObjectId(user.profilePicture));
+      const existingFile = await gridfsBucket
+        .find({ _id: new mongoose.Types.ObjectId(user.profilePicture) })
+        .toArray();
+
+      // Add a log to check the existing profile picture details
+      console.log("Checking if profile picture exists:", existingFile);
+
+      if (existingFile.length > 0) {
+        await gridfsBucket.delete(new mongoose.Types.ObjectId(user.profilePicture));
+      } else {
+        console.warn(`File not found for id ${user.profilePicture}, skipping delete.`);
+      }
     }
 
-    user.profilePicture = new mongoose.Types.ObjectId(req.file.id);
-    const savedUser = await user.save();
-    const tokens = createTokens(savedUser);
+    const filename = crypto.randomBytes(16).toString("hex") + path.extname(req.file.originalname);
 
-    res.status(200).json({
-      accessToken: tokens.accessToken,
+    // Upload the new profile picture to GridFS
+    const uploadStream = gridfsBucket.openUploadStream(filename, {
+      contentType: req.file.mimetype
     });
+    uploadStream.end(req.file.buffer);
+
+    uploadStream.on('finish', async () => {
+      // Save the new file ID to the user profile
+      user.profilePicture = new mongoose.Types.ObjectId(uploadStream.id);
+      const savedUser = await user.save();
+      const tokens = createTokens(savedUser);
+
+      res.status(200).json({
+        accessToken: tokens.accessToken,
+      });
+    });
+
+    uploadStream.on('error', (err) => {
+      console.error("Error during file upload:", err);
+      res.status(500).send({ error: "Error uploading file", err });
+    });
+
   } catch (err) {
+    console.error("Error in profile picture upload process:", err);
     res.status(500).send({ error: "Failed to upload profile picture", err });
   }
 };
 
 const get_profile_picture = async (req, res) => {
   try {
-    const connection = await mongoose.connection;
-    const db = connection.getClient().db();
-    let gridfsBucket = new mongoose.mongo.GridFSBucket(db, {
-      bucketName: "profilePicture",
+    const db = mongoose.connection.db;
+    const gridfsBucket = new mongoose.mongo.GridFSBucket(db, {
+      bucketName: "profilePicture"
     });
 
     const files = await gridfsBucket
@@ -247,22 +277,25 @@ const get_profile_picture = async (req, res) => {
   }
 };
 
-const delete_profile_picture = (req, res) => {
-  let gridfsBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-    bucketName: "profilePicture",
-  });
+const delete_profile_picture = async (req, res) => {
+  try {
+    const db = mongoose.connection.db;
+    const gridfsBucket = new mongoose.mongo.GridFSBucket(db, {
+      bucketName: "profilePicture"
+    });
 
-  User.findById(res.locals.user._id)
-    .then((user) => {
-      if (user.profilePicture) {
-        gridfsBucket.delete(new mongoose.Types.ObjectId(user.profilePicture));
-        user.profilePicture = undefined;
-        return user.save().then(() => res.sendStatus(200));
-      } else {
-        return res.sendStatus(204);
-      }
-    })
-    .catch((err) => res.send(err));
+    const user = await User.findById(res.locals.user._id);
+    if (user && user.profilePicture) {
+      await gridfsBucket.delete(new mongoose.Types.ObjectId(user.profilePicture));
+      user.profilePicture = undefined;
+      await user.save();
+      return res.sendStatus(200);
+    } else {
+      return res.sendStatus(204); // No content to delete
+    }
+  } catch (err) {
+    res.status(500).send({ error: "Failed to delete profile picture", err });
+  }
 };
 
 module.exports = {
--- mygridfs.js
+++ mygridfs.js
@@ -1,37 +1,14 @@
-const { GridFsStorage } = require('multer-gridfs-storage');
 const multer = require('multer');
-const crypto = require('crypto');
-const path = require('path');
-require("dotenv").config();
+const mongoose = require('mongoose');
 
-const dbUrl = process.env.DBURL;
-
-// Profile Picture Storage
-const profilePictureStorage = new GridFsStorage({
-    url: dbUrl,
-    options: {useUnifiedTopology: true},
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) {
-                    return reject(err);
-                }
-                const filename = buf.toString("hex") + path.extname(file.originalname);
-                const fileInfo = {
-                    filename: filename,
-                    bucketName: "profilePicture",
-                };
-                resolve(fileInfo);
-            });
-        });
-    }
-});
+// Use memory storage to temporarily store the file in memory before uploading to GridFS
+const storage = multer.memoryStorage();
 
 const uploadProfilePicture = multer({
-    storage: profilePictureStorage,
-    limits: { fileSize: 1000000}
+    storage,
+    limits: { fileSize: 1000000 } // 1MB limit
 });
 
 module.exports = {
     uploadProfilePicture
-}
+};
